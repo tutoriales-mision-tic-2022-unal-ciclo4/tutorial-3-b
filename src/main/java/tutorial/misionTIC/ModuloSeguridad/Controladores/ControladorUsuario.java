c8a67d52480d03b9c6d6c319f13a8b4b8f3321ca
felipebuitragocarmona <felipebuitragocarmona@gmail.com>
2022-04-30 12:24:52 -0500

    first commit

---
+++ src/main/java/tutorial/misionTIC/ModuloSeguridad/Controladores/ControladorUsuario.java
@@ -0,0 +1,70 @@
+package tutorial.misionTIC.ModuloSeguridad.Controladores;
+import tutorial.misionTIC.ModuloSeguridad.Modelos.Usuario;
+import tutorial.misionTIC.ModuloSeguridad.Repositorios.RepositorioUsuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+@CrossOrigin
+@RestController
+@RequestMapping("/usuarios")
+public class ControladorUsuario {
+    @Autowired
+    private RepositorioUsuario miRepositorioUsuario;
+
+    @GetMapping("")
+    public List<Usuario> index(){
+        return this.miRepositorioUsuario.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Usuario create(@RequestBody  Usuario infoUsuario){
+        infoUsuario.setContrasena(convertirSHA256(infoUsuario.getContrasena()));
+        return this.miRepositorioUsuario.save(infoUsuario);
+    }
+    @GetMapping("{id}")
+    public Usuario show(@PathVariable String id){
+        Usuario usuarioActual=this.miRepositorioUsuario
+                .findById(id)
+                .orElseThrow(RuntimeException::new);
+        return usuarioActual;
+    }
+    @PutMapping("{id}")
+    public Usuario update(@PathVariable String id,@RequestBody  Usuario infoUsuario){
+        Usuario usuarioActual=this.miRepositorioUsuario
+                .findById(id)
+                .orElseThrow(RuntimeException::new);
+        usuarioActual.setSeudonimo(infoUsuario.getSeudonimo());
+        usuarioActual.setCorreo(infoUsuario.getCorreo());
+        usuarioActual.setContrasena(convertirSHA256(infoUsuario.getContrasena()));
+        return this.miRepositorioUsuario.save(usuarioActual);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        Usuario usuarioActual=this.miRepositorioUsuario
+                .findById(id)
+                .orElseThrow(RuntimeException::new);
+        this.miRepositorioUsuario.delete(usuarioActual);
+    }
+    public String convertirSHA256(String password) {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+        byte[] hash = md.digest(password.getBytes());
+        StringBuffer sb = new StringBuffer();
+        for(byte b : hash) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
